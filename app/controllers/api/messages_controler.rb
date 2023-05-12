47da69f20986ff1bd1a9b143ea614a82300f1279
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-05-12 23:07:26 +0200

    fix errors

--- app/controllers/api/messages_controler.rb
+++ app/controllers/api/messages_controler.rb
@@ -1,10 +1,10 @@
 module Api
-    module V1
-      class MessagesController < ApplicationController
-        def index
-          message = Message.order('RANDOM()').first
-          render json: { message: message.message }
-        end
+  module V1
+    class MessagesController < ApplicationController
+      def index
+        message = Message.order('RANDOM()').first
+        render json: { message: message.message }
       end
     end
   end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,8 +7,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Message.create(message: "Hello, world!")
-Message.create(message: "How are you doing?!")
-Message.create(message: "Bonjour!")
-Message.create(message: "Welcome!")
-Message.create(message: "Willkommen!")
+Message.create(message: 'Hello, world!')
+Message.create(message: 'How are you doing?!')
+Message.create(message: 'Bonjour!')
+Message.create(message: 'Welcome!')
+Message.create(message: 'Willkommen!')
